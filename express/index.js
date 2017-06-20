c37600b3eb6d400873a2447fc2f1e4ed44833da0
vonP <thomas@thiebaudet.fr>
2017-06-20 12:01:28 +0200

    template with pug

--- express/index.js
+++ express/index.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const logger = require('./lib/logger');
+const path = require('path');
 
 app = express();
+
+app.use(logger);
+app.use(express.static(path.join(__dirname, 'public')));
+
+console.log(logger.version);
+
+app.get('/api',(req,res)=>{
+    res.json({message: 'Hi everybody'});
+});
+
+app.get('/', function (req, res) {
+    console.log('Homepage');
+    res.send('<h1>Welcome</h1>');
+});
 
 app.listen(8000);
