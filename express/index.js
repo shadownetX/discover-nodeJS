ff0b748852d0192f1195069b26c1bae0cc2df0f2
vonP <thomas@thiebaudet.fr>
2017-06-20 12:38:00 +0200

    création layout pug

--- express/index.js
+++ express/index.js
@@ -4,8 +4,15 @@ const path = require('path');
 
 app = express();
 
+// Utilisation du middleware logger
 app.use(logger);
+// On définit le chemin vers le dossier public
 app.use(express.static(path.join(__dirname, 'public')));
+
+// On définit le chemin vers le dossier de template
+app.set('views', path.join(__dirname,'views'));
+// On définit le moteur de template
+app.set('view engine','pug');
 
 console.log(logger.version);
 
@@ -15,7 +22,9 @@ app.get('/api',(req,res)=>{
 
 app.get('/', function (req, res) {
     console.log('Homepage');
-    res.send('<h1>Welcome</h1>');
+
+    // Passer des paramètres
+    res.render('home',{title:"Pug Life"});
 });
 
 app.listen(8000);
