d8ffa3db94af19784d23114e231bef6a784f9c37
vonP <thomas@thiebaudet.fr>
2017-06-21 13:35:52 +0200

    API fonctionnelle : create, update, remove, getAll, findOne

--- api/routes/todo.js
+++ api/routes/todo.js
@@ -2,8 +2,8 @@
 
 const controllers = require('../controllers/todo.js');
 
-router.get('/todo/tasks',controllers.all);
-router.get('/todo/tasks/:id',controllers.find);
-router.put('/todo/tasks/:id',controllers.update);
-router.post('/todo/tasks',controllers.create);
-router.delete('/todo/tasks/:id',controllers.remove);
+router.get('/todo/tasks', controllers.all);
+router.get('/todo/tasks/:id', controllers.find);
+router.put('/todo/tasks/:id', controllers.update);
+router.post('/todo/tasks', controllers.create);
+router.delete('/todo/tasks/:id', controllers.remove);
