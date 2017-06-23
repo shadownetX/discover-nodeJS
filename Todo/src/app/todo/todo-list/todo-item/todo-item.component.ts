3b525870caa2662fc96f2230e95d2be0f76439f8
vonP <thomas@thiebaudet.fr>
2017-06-23 16:08:52 +0200

    fin de journée

--- Todo/src/app/todo/todo-list/todo-item/todo-item.component.ts
+++ Todo/src/app/todo/todo-list/todo-item/todo-item.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,Input, OnInit } from '@angular/core';
 
+import { Task } from '../../task';
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent implements OnInit {
+
+@Input()
+task: Task;
 
   constructor() { }
 
